9c56425e597f46367eb0be94f7b49ef618666f6a
adem.kaya <adem0334>
2022-01-08 22:04:08 +0300

    blog file 4

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views.generic import ListView, TemplateView, DetailView
@@ -19,10 +19,22 @@ class PostDetail(DetailView):
     model = Post
     context_object_name = 'single'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):                                       # *args sınırsız sayıda parametreli fonksiyon oluşturmak için
         context = super(PostDetail, self).get_context_data(**kwargs)
         return context
 
 
 class CategoryDetail(ListView):
-    pass
+    model = Post   # postları listeleyeceğiz o yüzden modelimiz catogory değil
+    template_name = 'categories/category_detail.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return Post.objects.filter(category=self.category).order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetail, self).get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return context
+
